bd72ebb9c78c1ec8d9d712e70856076e5ea278fa
Ralina <seitovaralina1@gmail.com>
2023-10-08 06:50:14 +0000

    Код с ПЗ

---
+++ spacebattle/SpaceBattle.Lib/Move.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMovable
+{
+    Vector Position { get; set; }
+    Vector Velocity { get; }
+}
+
+public class MoveCommand : ICommand
+{
+    private IMovable movable;
+    public MoveCommand(IMovable movable)
+    {
+        this.movable = movable;
+    }
+
+    public void Execute()
+    {
+        movable.Position = movable.Position + movable.Velocity;
+    }
+}
