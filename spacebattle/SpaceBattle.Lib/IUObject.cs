156fb877baeec7d4e3d1b0fec3247fadbd1f9837
Ralina <seitovaralina1@gmail.com>
2023-12-15 22:59:23 +0000

    deleted extra files

--- spacebattle/SpaceBattle.Lib/IUObject.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpaceBattle.Lib;
-
-public interface IUObject
-{
-    public object GetProperty(string name);
-    public void SetProperty(string name, object value);
-    public void DeleteProperty(string name);
-}
