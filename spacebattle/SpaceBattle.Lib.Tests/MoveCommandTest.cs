fcc353d146908ef5216fa3b21942edbb2f531df4
Egor-Kozh <kozhevnikovegor55@gmail.com>
2023-12-28 17:02:59 +0600

    FirstTest

--- spacebattle/SpaceBattle.Lib.Tests/MoveCommandTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/MoveCommandTest.cs
@@ -8,8 +8,8 @@ public class MoveCommandTest
         // pre
         var movable = new Mock<IMovable>();
 
-        movable.SetupGet(m => m.Position).Returns(new int[] { 12, 5 }).Verifiable();
-        movable.SetupGet(m => m.Velocity).Returns(new int[] { -7, 3 }).Verifiable();
+        movable.SetupGet(m => m.Position).Returns(new int[] {12, 5}).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new int[] {-7, 3}).Verifiable();
 
         ICommand moveCommand = new MoveCommand(movable.Object);
 
@@ -18,7 +18,7 @@ public class MoveCommandTest
 
         //post
         // movable.Position is correct
-        movable.VerifySet(m => m.Position = new int[] { 5, 8 }, Times.Once);
+        movable.VerifySet(m => m.Position = new int[] {5, 8}, Times.Once);
         movable.VerifyAll(); 
     }
 }
