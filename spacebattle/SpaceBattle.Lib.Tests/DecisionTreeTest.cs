38e7b263867e6b3e1a76478c4b9b82a678440cce
Egor-Kozh <kozhevnikovegor55@gmail.com>
2024-05-15 11:39:28 +0600

    DTTest

--- spacebattle/SpaceBattle.Lib.Tests/DecisionTreeTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/DecisionTreeTest.cs
@@ -6,5 +6,24 @@ using Moq;
 
 public class DecisionTreeTest
 {
-    
+    readonly string path;
+
+    public DecisionTreeTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<ICommand>(
+            "Scopes.Current.Set", 
+            IoC.Resolve<object>("Scopes.New", 
+            IoC.Resolve<object>("Scopes.Root")))
+        .Execute();
+
+        var tree = new Dict();
+        IoC.Resolve<ICommand>(
+            "IoC.Register", "Game.DecisionTree", 
+            (object[] args) => tree
+        ).Execute();
+
+        path = @"../../../Vectors.txt";
+    }
 }
