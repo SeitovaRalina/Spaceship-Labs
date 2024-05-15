99b76ff987a7eb917716ab41240a4cf16cbcf6ce
Egor-Kozh <kozhevnikovegor55@gmail.com>
2024-05-15 11:09:53 +0600

    DecTreeExe

--- spacebattle/SpaceBattle.Lib/DecisionTree.cs
+++ spacebattle/SpaceBattle.Lib/DecisionTree.cs
@@ -9,6 +9,23 @@ public class DecisionTree : ICommand
 
     public void Execute()
     {
-        
+        private readonly string _path;
+        public DecisionTree(string path) => _path = path;
+
+        public void Execute()
+        {
+            var vectors = File.ReadAllLines(_path).Select(
+                line => line.Split().Select(int.Parse).ToList()).ToList();
+
+            var tree = IoC.Resolve<Dict>("Game.DecisionTree");
+
+            vectors.ForEach(vector => {
+                var layer = tree;
+                vector.ForEach(num => {
+                    layer.TryAdd(num, new Dict()); 
+                    layer = (Dict) layer[num];
+                });
+            });
+        }
     }
 }
