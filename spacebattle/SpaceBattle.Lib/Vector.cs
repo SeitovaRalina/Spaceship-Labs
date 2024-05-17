d6d41ceb3cc3d63d7c854ce34cf80384cb741c32
SeitovaRalina <124547106+SeitovaRalina@users.noreply.github.com>
2024-05-18 02:49:04 +0600

    Исправления ... (#13)
    * fix : yml warning (from-node-16-to-node-20)
    
    * feat : added Vector
    
    * fix : yml warning
    
    * fix : fixed methods in Vector

---
+++ spacebattle/SpaceBattle.Lib/Vector.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SpaceBattle.Lib;
+
+[ExcludeFromCodeCoverage]
+public class Vector
+{
+    private int[] coordinates;
+    private int this[int i]
+    {
+        get => coordinates[i];
+        set => coordinates[i] = value;
+    }
+    public Vector(int[] coordinates)
+    {
+        this.coordinates = coordinates;
+    }
+    public Vector(int length)
+    {
+        coordinates = new int[length];
+    }
+    private int Length()
+    {
+        return coordinates.Length;
+    }
+    public static Vector operator +(Vector x, Vector y)
+    {
+        var temp = new Vector(x.Length())
+        {
+            coordinates = x.coordinates.Select((value, index) => value + y[index]).ToArray()
+        };
+        return temp;
+    }
+    public override bool Equals(object? obj)
+    {
+        return obj != null && coordinates.SequenceEqual(((Vector)obj).coordinates);
+    }
+    public override int GetHashCode()
+    {
+        return coordinates.GetHashCode();
+    }
+}
