197be5bec3810c9bfa61d502fea5f249e5df1811
Ralina <seitovaralina1@gmail.com>
2024-05-17 20:07:12 +0000

    feat : added vector

---
+++ spacebattle/SpaceBattle.Lib/Vector.cs
@@ -0,0 +1,34 @@
+﻿namespace SpaceBattle.Lib;
+
+public class Vector
+{
+    private readonly int[] coordinates;
+    private int this[int i]
+    {
+        get => coordinates[i];
+        set => coordinates[i] = value;
+    }
+    public Vector(int[] coordinates)
+    {
+        this.coordinates = coordinates;
+    }
+    public Vector(int length)
+    {
+        coordinates = new int[length];
+    }
+    private int Length()
+    {
+        return coordinates.Length;
+    }
+
+    public static Vector operator +(Vector x, Vector y)
+    {
+        var temp = new Vector(x.Length());
+        for (var i = 0; i < temp.Length(); i++)
+        {
+            temp[i] = x[i] + y[i];
+        }
+
+        return temp;
+    }
+}
