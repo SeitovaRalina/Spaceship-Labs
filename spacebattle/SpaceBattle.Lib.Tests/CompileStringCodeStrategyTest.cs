7515a4edb6303cf23006c2ccf13c3f50fd76c332
Ralina <seitovaralina1@gmail.com>
2024-05-22 06:51:36 +0000

    test : added all tests

---
+++ spacebattle/SpaceBattle.Lib.Tests/CompileStringCodeStrategyTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Microsoft.CodeAnalysis;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class CompileStringCodeStrategyTest
+{
+    public CompileStringCodeStrategyTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "Scopes.Current.Set",
+            IoC.Resolve<object>(
+                "Scopes.New",
+                IoC.Resolve<object>("Scopes.Root")
+            )
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Code.Compile",
+            (object[] args) => new CompileStringCodeStrategy().Init(args)
+        ).Execute();
+
+    }
+    [Fact]
+    public void SuccessfulCompilationStringCode()
+    {
+        var assemblyName = "String Code Compilation";
+        var references = new List<MetadataReference> {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(Assembly.LoadFrom("SpaceBattle.Lib.dll").Location)
+        };
+        var code = @"namespace SpaceBattle.Lib;
+                    public class FakeClass
+                    {
+                        static int Square(int n) => n * n;
+                    }";
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Code.Compile.AssemblyName",
+            (object[] args) => assemblyName
+        ).Execute();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Code.Compile.References",
+            (object[] args) => references
+        ).Execute();
+
+        var assembly = IoC.Resolve<Assembly>("Code.Compile", code);
+
+        var t = assembly.GetType("SpaceBattle.Lib.FakeClass");
+        var instance = Activator.CreateInstance(t!)!;
+
+        var square = t!.GetMethod("Square", BindingFlags.NonPublic | BindingFlags.Static);
+        var result = square?.Invoke(null, new object[] { 7 });
+
+        Assert.Equal("SpaceBattle.Lib.FakeClass", instance.GetType().ToString());
+        Assert.Equal(49, result);
+    }
+}
---
+++ spacebattle/SpaceBattle.Lib.Tests/FindGameAdapterStrategyTest.cs
@@ -0,0 +1,91 @@
+﻿using System.Reflection;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Microsoft.CodeAnalysis;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class FindGameAdapterStrategyTest
+{
+    public FindGameAdapterStrategyTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "Scopes.Current.Set",
+            IoC.Resolve<object>(
+                "Scopes.New",
+                IoC.Resolve<object>("Scopes.Root")
+            )
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Code.Compile.AssemblyName",
+            (object[] args) => "Game Adapter Code Compilation"
+        ).Execute();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Code.Compile.References",
+            (object[] args) => new List<MetadataReference> {
+                            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                            MetadataReference.CreateFromFile(Assembly.LoadFrom("SpaceBattle.Lib.dll").Location)}
+        ).Execute();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Code.Compile",
+            (object[] args) => new CompileStringCodeStrategy().Init(args)
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Game.Adapter.Name",
+            (object[] args) => args[0].ToString() + "Adapter"
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Game.Adapter.Find",
+            (object[] args) => new FindGameAdapterStrategy().Init(args)
+        ).Execute();
+    }
+    [Fact]
+    public void SuccessfulFindingGameAdapter()
+    {
+        var uObject = new Mock<IUObject>();
+        uObject.Setup(x => x.GetProperty("Angle")).Returns(new VectorAngle(60));
+
+        var targetType = typeof(ITurnable);
+
+        var adapterCode = @"namespace SpaceBattle.Lib;
+
+public class ITurnableAdapter : ITurnable
+{
+    private readonly IUObject _uObject;
+    public ITurnableAdapter(IUObject uObject)
+    {
+        _uObject = uObject;
+    }
+
+    public VectorAngle Angle { get => (VectorAngle)_uObject.GetProperty(""Angle""); 
+    set => _uObject.SetProperty(""Angle"", value); }
+
+    public VectorAngle DeltaAngle => (VectorAngle)_uObject.GetProperty(""DeltaAngle"");
+}
+        ";
+        var asmDict = new Dictionary<KeyValuePair<Type, Type>, Assembly>
+        {
+            [new KeyValuePair<Type, Type>(uObject.Object.GetType(), targetType)] = IoC.Resolve<Assembly>("Code.Compile", adapterCode)
+        };
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Game.Adapter.Assemblies.Dictionary",
+            (object[] args) => asmDict
+        ).Execute();
+
+        var adapter = (ITurnable)IoC.Resolve<object>("Game.Adapter.Find", uObject.Object, targetType);
+
+        Assert.NotNull(adapter);
+        Assert.Equal("ITurnableAdapter", adapter.GetType().Name);
+        Assert.Equal(new VectorAngle(60), adapter.Angle);
+    }
+}
