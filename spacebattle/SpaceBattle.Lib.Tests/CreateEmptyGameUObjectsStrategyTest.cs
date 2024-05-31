b1565ac60bb17411ed717f42f30b99c1e7768eaf
Ralina <seitovaralina1@gmail.com>
2024-05-31 03:34:34 +0000

    feat : adding GameUObjectsGenerator

--- spacebattle/SpaceBattle.Lib.Tests/CreateEmptyGameUObjectsStrategyTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/CreateEmptyGameUObjectsStrategyTest.cs
@@ -3,9 +3,9 @@ using Hwdtech.Ioc;
 
 namespace SpaceBattle.Lib.Tests;
 
-public class CreateEmptyGameUObjectsCommandTest
+public class CreateEmptyGameUObjectsStrategyTest
 {
-    public CreateEmptyGameUObjectsCommandTest()
+    public CreateEmptyGameUObjectsStrategyTest()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>(
@@ -19,28 +19,31 @@ public class CreateEmptyGameUObjectsCommandTest
         IoC.Resolve<Hwdtech.ICommand>(
             "IoC.Register",
             "Game.UObjects.CreateEmpty",
-            (object[] args) => new CreateEmptyGameUObjectsCommand((int)args[0])
-        ).Execute();
-
-        IoC.Resolve<Hwdtech.ICommand>(
-            "IoC.Register",
-            "Game.UObject.Create",
-            (object[] args) => new Mock<IUObject>().Object
+            (object[] args) => new CreateEmptyGameUObjectsStrategy().Init(args)
         ).Execute();
     }
     [Fact]
     public void SuccessfulCreatingEmptyGameUObjects()
     {
-        var uObjectsCount = 5;
+        var uObjectsCount = 3;
+
+        var emptyUObject = new Mock<IUObject>();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Game.UObject.CreateEmpty",
+            (object[] args) => emptyUObject.Object
+        ).Execute();
 
         var uObjectsDict = new Dictionary<int, IUObject>();
         IoC.Resolve<Hwdtech.ICommand>(
             "IoC.Register",
-            "Game.UObjects.Dictionary",
-            (object[] args) => uObjectsDict
+            "Game.UObject.Add",
+            (object[] args) => new ActionCommand(() => { uObjectsDict.Add((int)args[0], (IUObject)args[1]); })
         ).Execute();
 
-        IoC.Resolve<ICommand>("Game.UObjects.CreateEmpty", uObjectsCount).Execute();
+        var generatedUObjects = IoC.Resolve<IEnumerable<IUObject>>("Game.UObjects.CreateEmpty", uObjectsCount);
+
+        Assert.True(generatedUObjects.All(obj => obj == emptyUObject.Object));
 
         Assert.Equal(uObjectsCount, uObjectsDict.Count);
         Assert.True(uObjectsDict.Keys.SequenceEqual(Enumerable.Range(0, uObjectsCount)));
